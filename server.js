59153847b98b6b158450e5e89f56da865722a891
devintyler26 <devincunningham@allegropediatrics.com>
2018-01-06 15:12:52 -0800

    testing hard coded table creation

--- server.js
+++ server.js
@@ -8,17 +8,38 @@ const bodyParser = require('body-parser')
 
 const PORT = process.env.PORT || 3000
 
-// const conString = 'postgres://postgres:1234@localhost:5432/doggoes'
+const DATABASE_URL = process.env.DATABASE_URL || 'postgres://postgres:1234@localhost:5432/books_app'
 
-// const client = new pg.Client(conString)
+const client = new pg.Client(DATABASE_URL)
 
-// client.connect()
+client.connect()
 
 app.get('/test', (req, res) => res.send('hello world'))
 
 app.listen(PORT, () => {console.log(`server listening on ${PORT}`)})
 
-// client.query('SELECT * FROM books').then((result)=> {console.log(result.rows[0])})
+client.query('SELECT * FROM books').then((result)=> {console.log(result.rows[0])})
 
 app.use(bodyParser.urlencoded({extended:true}))
 
+app.get('/books', (req, res) =>{
+    client.query(`
+    SELECT * FROM books
+    `).then(results => res.send(results.rows))
+    .catch(err => console.log(err))
+})
+
+client.query(`
+    CREATE TABLE if not exists books(
+        id serial primary key,
+        author VARCHAR(255),
+        title VARCHAR(255),
+        isbn VARCHAR(255),
+        image_url VARCHAR(255),
+        description text
+    );
+`)
+client.query(`
+    INSERT INTO books(author, title, isbn, image_url, description)
+    VALUES('author1', 'title1', 'isbn1', 'http://www.placecage.com/300/200', 'this is a book');
+`)
