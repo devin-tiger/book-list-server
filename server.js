7ea8434dd8cade04637ca4b5ed235c61d39ac49d
devintyler26 <devincunningham@allegropediatrics.com>
2018-01-07 17:27:10 -0800

    testing new route

--- server.js
+++ server.js
@@ -33,8 +33,16 @@ app.get('/api/v1/books', (req, res) =>{
 
 app.get('/', (request, response) => {
     response.sendFile('index.html', {root: '../book-list-client'});
-  });
-
+    
+    function showBooks(results){
+        var $ul = $('#books-list')
+        $ul.empty()
+        results.forEach(books =>{
+            $ul.append(`<li> ${books.author}, ${books.title}, ${books.isbn}, ${books.img_url}</li>`)
+        })
+    }
+});
+    
 
 // client.query(`
 //     CREATE TABLE if not exists books(
