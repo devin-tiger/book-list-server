3bbb1c01b3a2bc33310cc7a39337e7397ef038be
devintyler26 <devincunningham@allegropediatrics.com>
2018-01-13 15:32:12 -0800

    changing syntax

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.post('/api/v1/books', express.json(), express.urlencoded({extended:true}), (
 app.put('/api/v1/books/:id', express.json(), express.urlencoded({extended:true}), (req, res) =>{
     client.query(`
     UPDATE books
-        SET (title = '$1', author = '$2', isbn = '$3', image_url = '$4', description = '$5')
+        SET title = $1, author = $2, isbn = $3, image_url = $4, description = $5
         WHERE id=${req.params.id}
     `,[
         req.body.title,
